38040e13a341e0543f9b88e13ae1022d3ae30793
Aayasha Khatun <167515078+Aayasha786@users.noreply.github.com>
2024-08-29 16:26:18 +0545

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import LoginSignup from './component/LoginSignup'
+
+function App() {
+ 
+
+  return (
+    // <>
+    <LoginSignup/>
+
+      
+    // </>
+  )
+}
+
+export default App
